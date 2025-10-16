f1b8f4d59592fc823e03b43d7c444d17c10b1a90
Alomyr <matheuscasto11213@gmail.com>
2025-10-16 15:58:52 -0300

    Add README and improve comments in Repositorio class

--- src/main/java/com/atividadeDePratica/lp2/Repositorio.java
+++ src/main/java/com/atividadeDePratica/lp2/Repositorio.java
@@ -5,19 +5,19 @@ import java.util.List;
 public class Repositorio<T> {
     private List<T> objetos;
 
-    public Repositorio() {
+    public Repositorio() { ///Metodo Construtor
         this.objetos = new ArrayList<>();
     }
 
-    public void adicionar(T obj) { //metodo de adicao
+    public void adicionar(T obj) { //Metodo de Adição
         objetos.add(obj);
     }
 
-    public void remover(T obj) { //metodo de remocao 
+    public void remover(T obj) { //Metodo de Remoção
         objetos.remove(obj);
     }
 
-    public T buscar(String chave) {
+    public T buscar(String chave) { ///Metodo de Buscar por chave usando Generic
         for (T obj : objetos) {
             if (obj.toString().contains(chave)) {
                 return obj;
@@ -25,8 +25,8 @@ public class Repositorio<T> {
         }
         return null;
     }
-
-    public void listar() {
+    
+    public void listar() { ///Metodo de  Listagem usando Generic
         for (T obj : objetos) {
             System.out.println(obj);
         }
